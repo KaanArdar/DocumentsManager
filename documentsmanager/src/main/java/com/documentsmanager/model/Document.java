409674bce90ab20d916ae38e059cf719f9e53043
KAANARDAR\Kaan <ardarkaan@gmail.com>
2018-01-24 00:26:14 +0300

    ->add Document model

---
+++ documentsmanager/src/main/java/com/documentsmanager/model/Document.java
@@ -0,0 +1,15 @@
+package com.documentsmanager.model;
+
+public class Document {
+
+	private DocumentBody _source;
+
+	public DocumentBody get_source() {
+		return _source;
+	}
+
+	public void set_source(DocumentBody _source) {
+		this._source = _source;
+	}
+
+}
